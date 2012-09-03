8be514cde1876810fa7bdd57dea53dcb796a654b
Jeffrey Hsu <jeffhsu3@gmail.com>
2012-09-03 02:34:07 -0400

    Finished PEP8 changes and placed all the genotypes into one function.
    The reason this was done was to put the function in a central location for easier changes in the future.  The current output was checked against the original and it matches.

--- convert_vcf.py
+++ convert_vcf.py
@@ -3,13 +3,12 @@
 usage:
     python convert_vcf blast_file vcf_file
 """
+
 import sys
 import optparse
 
 COORD_CHANGES = {}
 
-#TODO: Add an opton to input a file with a list of bad loci
-BAD_LOCI = []
 
 def change_coord(pos, coord_map = COORD_CHANGES):
     change = 0
@@ -21,41 +20,53 @@ def change_coord(pos, coord_map = COORD_CHANGES):
 
 def new_snp(pos, numppl, d):
     s = 'chrM'#TODO: find a way to figure out from vcf
-    s += '\t'+str(d[pos][0])
+    s += '\t' + str(d[pos][0])
     s += '\t.'
-    s += '\t'+d[pos][2]+'\t'+d[pos][1]
-    s += '\t.\t.\t.\t.'
+    s += '\t' + d[pos][2] + '\t' + d[pos][1]
+    s += '\t.' * 4
     for i in range(numppl):
         s += '\t1/1'
     s += '\n'
     return s
 
 def new_snp_pos(d,c):
-    """
-    """
     l = sorted(list(d.keys()))
     for i in l:
         if i not in c:
             return i
     return 'Done'
 
-def convert_genotypes(samples):
+def convert_genotypes(samples, multiple_alleles = False):
     """ Converts the genotypes of all samples in a given entry
     and returns a list of vcfentry.
+
+    :TODO The multiple conversion isn't quite right
     """
     ppl = []
     for p in samples:
         p = p.split(':')
         freqs = p[1].split(',')[1] + ',' + p[1].split(',')[0]
+        if multiple_alleles:
+            freqs = '0,' + freqs
         if p[0] == '0/0':
-            ppl.append('1/1:' + freqs + ':' + ':'.join(p[2:]))
-        elif p[0] == '1/1':
+            if multiple_alleles:
+                temp = '2/2'
+            else:
+                temp = '1/1'
+            ppl.append(temp + ':' + freqs + ':' + ':'.join(p[2:]))
+        elif p[0] == '1/1' and not multiple_alleles:
             ppl.append('0/0:' + freqs + ':' + ':'.join(p[2:]))
-        else:#1/0 or ./., neither of which need changing
+        elif p[0] in ['1/0', '0/1'] and multiple_alleles:
+            ppl.append('1/2:' + freqs + ':' + ':'. join(p[2:]))
+        else: #1/0 or ./., neither of which need changing
             ppl.append(':'.join(p))
     return ppl
 
+
 if __name__ == '__main__':
+    #TODO: Add an opton to input a file with a list of bad loci
+    BAD_LOCI = []
+
     c =  open(sys.argv[1] + '.changes','rU')
     s =  open(sys.argv[1] + '.shifts','rU')
     vcf = open(sys.argv[2],'rU')
@@ -73,10 +84,6 @@ if __name__ == '__main__':
 
     debug = 0
     changes=[]
-
-    print('yo')
-
-
 
     for line in vcf:
         if line[0] == '#':
@@ -102,20 +109,11 @@ if __name__ == '__main__':
                     ppl = convert_genotypes(line[9:])
                 elif len(line[4].split(',')) == 1:
                     alt = line[4] + ',' + oldref
-                    for p in line[9:]:
-                        p = p.split(':')
-                        freqs = '0,' + p[1].split(',')[1] + ',' +\
-                                p[1].split(',')[0]
-                        if p[0] == '0/0':
-                            ppl.append('2/2:' + freqs+':' + ':'.join(p[2:]))
-                        elif p[0] in ['1/0','0/1']:
-                            ppl.append('1/2:' + freqs + ':' + ':'.join(p[2:]))
-                        else:#1/1 or ./., neither of which need changing
-                            ppl.append(':'.join(p))
+                    ppl = convert_genotypes(line[9:], multiple_alleles = True)
                 elif newref in line[4].split(','):
                     switch = line[4].split(',').index(newref)
                     alt = ','.join([oldref if x==newref else x for x in line[4].split(',')])
-                    def switch_indexes(n,s):
+                    def switch_indexes(n, s):
                         if n == s:
                             return 0
                         elif n == 0:
@@ -123,19 +121,23 @@ if __name__ == '__main__':
                         return n
                     for p in line[9:]:
                         p = p.split(':')
-                        freqs=','.join([p[1].split(',')[switch_indexes(n,switch+1)]\
+                        freqs = ','.join([p[1].split(',')[switch_indexes(n,switch+1)]\
                                         for n,x in enumerate(p[1].split(','))])
                         f = '/'.join([str(switch_indexes(x,switch+1)) for x in p[0].split('/')])
-                        ppl.append(f+':'+freqs+':'+':'.join(p[2:]))
+                        ppl.append(f + ':' + freqs + ':' + ':'.join(p[2:]))
                 else:
-                    alt = line[4]+','+oldref
+                    alt = line[4] + ','+ oldref
                     for p in line[9:]:
                         p = p.split(':')
-                        freqs = '0,'+p[1].split(',')[1]+','+p[1].split(',')[2]+','+p[1].split(',')[0]
+                        freqs = '0,'+ p[1].split(',')[1] + ',' + p[1].split(',')[2] +','\
+                                +p[1].split(',')[0]
+
                         f = '/'.join([x if x != '0' else '3' for x in p[0].split('/')])
                         ppl.append(f+':'+freqs+':'+':'.join(p[2:]))
 
-                w.write(line[0]+'\t'+str(newpos)+'\t'+line[2]+'\t'+newref+'\t'+alt+'\t'+'\t'.join(line[5:9])+'\t'+'\t'.join(ppl)+'\n')
+                w.write(line[0]+'\t'+str(newpos)+'\t'+line[2]+'\t'+newref+'\t'\
+                        +alt+'\t'+'\t'.join(line[5:9])+'\t'+'\t'.join(ppl)+'\n')
+
     while new_snp_pos(d,changes) != 'Done':
         w.write(new_snp(new_snp_pos(d,changes), numppl, d))
         changes.append(new_snp_pos(d,changes))
